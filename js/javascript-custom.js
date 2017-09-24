32571e7ca7c494fccc74db2da669252664d1be8e
JainalArifin <zainalunpam05@gmail.com>
2017-09-24 17:02:03 +0700

    compeny profile semantic web

---
+++ js/javascript-custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var index = 0,
+      items = $('.container-image div'),
+      total = items.length;
+
+  function slide(){
+    var item = $('.container-image div').eq(index);
+    items.slideUp()
+    item.slideDown()
+  }
+
+  var autoslide = setInterval(function(){
+    index += 1;
+    if( index > total -1 ) index = 0
+    slide()
+  }, 4000);
+
+});
